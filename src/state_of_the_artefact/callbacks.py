df08f0c2d0e8ac83c6077a90ef5569541bc09c29
maxvstheuniverse <max.peeperkorn@protonmail.com>
2020-07-07 11:35:42 +0200

    cleanup

--- src/state_of_the_artefact/callbacks.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-
-
-class CustomCallback(tf.keras.callbacks.Callback):
-    def on_epoch_end(self, epoch, logs=None):
--- src/state_of_the_artefact/config.py
+++
@@ -1,4 +0,0 @@
-timesteps = 10
-original_dim = 12
-hidden_dim = 128
-latent_dim = 32
