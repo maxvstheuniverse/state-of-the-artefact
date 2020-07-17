427b3afa58948fd88b8228f3de4382ade4581666
maxvstheuniverse <max.peeperkorn@protonmail.com>
2020-07-17 11:58:14 +0200

    updated logs with notes

--- src/state_of_the_artefact/scripts/generate_gaussian_ints.py
+++ src/state_of_the_artefact/scripts/generate_gaussian_ints.py
@@ -4,17 +4,18 @@ import matplotlib.pyplot as plt
 
 x = np.arange(0, 88)
 xU, xL = x + .5, x - .5
-prob = ss.norm.cdf(xU, loc=60, scale=6) - ss.norm.cdf(xL, loc=60, scale=6)
+prob = ss.norm.cdf(xU, loc=24, scale=2) - ss.norm.cdf(xL, loc=35, scale=2)
 prob = prob / prob.sum()  # normalize the probabilities so their sum is 1
 nums = np.random.choice(x, size=100000, p=prob)
 
 xU2, xL2 = x + .5, x - .5
-prob2 = ss.norm.cdf(xU2, loc=21, scale=6) - ss.norm.cdf(xL2, loc=21, scale=6)
+prob2 = ss.norm.cdf(xU2, loc=60, scale=2) - ss.norm.cdf(xL2, loc=71, scale=2)
 prob2 = prob2 / prob2.sum()  # normalize the probabilities so their sum is 1
 nums2 = np.random.choice(x, size=100000, p=prob2)
 
 plt.hist(nums, bins=x)
 plt.hist(nums2, bins=x)
+plt.gca().set_ylim(0, 10000)
 plt.show()
 # print(nums + 44)
 
--- src/state_of_the_artefact/utilities.py
+++ src/state_of_the_artefact/utilities.py
@@ -12,9 +12,10 @@ def calculate_distances(x):
 
 
 def interpolate(a, b, nsteps):
-    """ nterpolates two arrays in the given number of steps. """
+    """ Interpolates two arrays in the given number of steps. """
     alpha = 1 / nsteps
     steps = []
+
     for i in range(1, nsteps + 1):
         alpha_star = alpha * i
         step = alpha_star * b + (1 - alpha_star) * a
@@ -36,5 +37,9 @@ def generate_integer_probabilities(minv, maxv, loc, scale):
     """ Returns a probabilitie distrubtion for the given range of integers. """
     x = np.arange(minv, maxv)
     xUb, xLb = x + 0.5, x - 0.5  # deviate the lower and upperbound for each integer
+
+    if not isinstance(loc, tuple):
+        loc = (loc, loc)
+
     prob = ss.norm.cdf(xUb, loc=loc, scale=scale) - ss.norm.cdf(xLb, loc=loc, scale=scale)
     return prob / prob.sum()  # return normalized probablities
